6c894e17f813078070e1dad9fd8c681011e62f89
Paul Amazona <whatevergeek@gmail.com>
2019-02-13 02:26:20 +0800

    Applied patch for Issue 15 - Applied patch to resolve https://github.com/DataKind-SG/wpdx/issues/15 - Modified .gitignore to add vscode settings - Modified wpdx_sample_data.csv to be based on the first few hundred rows of the main data - Refactored to be PEP8 compliant - Refactored fecal_coliform_presence column cleanup

--- data_cleaning/test_clean_wpdx_sample_data.py
+++ data_cleaning/test_clean_wpdx_sample_data.py
@@ -1,40 +1,43 @@
 import pytest
-import clean_wpdx_sample_data
+import clean_wpdx_sample_data as cwsd
 import pandas
+
 
 # @pytest.mark.skip
 def test_clean_col_country_name():
     """
     Test the cleaning for column: "country_name"
     """
-    assert clean_wpdx_sample_data.clean_col_country_name('NA') == 'NA'
+    assert cwsd.clean_col_country_name('NA') == 'NA'
+
 
 def test_clean_col_country_id():
     """
     Test the cleaning for column: "country_name"
     """
-    assert clean_wpdx_sample_data.clean_col_country_id('ph') == 'PH'
-    assert clean_wpdx_sample_data.clean_col_country_id('P2') == 'None'
+    assert cwsd.clean_col_country_id('ph') == 'PH'
+    assert cwsd.clean_col_country_id('P2') == 'None'
 
 
 def test_clean_col_install_year():
     """
     Test the cleaning for column: "install_year"
     """
-    assert clean_wpdx_sample_data.clean_col_install_year('2001.') == 2001
-    
+    assert cwsd.clean_col_install_year('2001.') == 2001
+
 
 def test_clean_col_fecal_coliform_presence():
     """
     Test the cleaning for column: "fecal_coliform_presence"
     """
-    assert pandas.isna(clean_wpdx_sample_data.clean_col_fecal_coliform_presence('junk'))
-    assert clean_wpdx_sample_data.clean_col_fecal_coliform_presence('Presence') == 'Presence'
-    assert clean_wpdx_sample_data.clean_col_fecal_coliform_presence('Absence') == 'Absence'
+    assert cwsd.clean_col_fecal_coliform_presence('junk') == 'NaN'
+    assert cwsd.clean_col_fecal_coliform_presence('Presence') == 'Presence'
+    assert cwsd.clean_col_fecal_coliform_presence('Absence') == 'Absence'
+
 
 def test_clean_col_adm1():
     """
     Test the cleaning for column: "adm1"
     """
-    assert clean_wpdx_sample_data.clean_col_adm1('singapore') == 'SINGAPORE'
-    assert clean_wpdx_sample_data.clean_col_adm1(' Singapore ') == 'SINGAPORE'
+    assert cwsd.clean_col_adm1('singapore') == 'SINGAPORE'
+    assert cwsd.clean_col_adm1(' Singapore ') == 'SINGAPORE'
